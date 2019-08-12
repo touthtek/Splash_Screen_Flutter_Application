0c559e4e8b2884762f2724852174c1a766b9bb15
Olu-johnson Oluwatobi Oluwatosin <touthtek@gmail.com>
2019-08-12 13:52:58 +0400

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:open_portal/screens/splashscreen.dart';
+import 'package:open_wallet/screens/splashscreen.dart';
 
 void main() => runApp(new MaterialApp(
       theme: ThemeData(
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:open_portal/screens/intro.dart';
+import 'package:open_wallet/screens/intro.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
